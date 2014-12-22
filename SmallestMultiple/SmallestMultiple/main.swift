5a2dde554a3f3817bce336ea1fd4e339f63ce2b0
Mehdi Sqalli <Mehdi@Mehdis-MacBook-Pro.local>
2014-12-22 23:05:37 +0100

    Add Project Euler Smallest Multiple

---
+++ SmallestMultiple/SmallestMultiple/main.swift
@@ -0,0 +1,41 @@
+//
+//  main.swift
+//  SmallestMultiple
+//
+//  Created by Mehdi Sqalli on 21/12/14.
+//  Copyright (c) 2014 MNT Developpement. All rights reserved.
+//
+
+import Foundation
+
+
+func getSmallestMultiple() {
+    
+    var result = 1
+    var ranges = [Int](1...20)
+    
+    for var i = 0; i < ranges.count; ++i {
+        
+        result *= ranges[i]
+        
+        for var j = i + 1; j < ranges.count; ++j {
+            if ranges[j] % ranges[i] == 0 {
+                ranges[j] = ranges[j] / ranges[i]
+            }
+        }
+        
+    }
+    
+    println(result)
+}
+
+func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    operation()
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    println("Time elapsed for \(title): \(timeElapsed) s")
+}
+
+printTimeElapsedWhenRunningCode("Smallest Multiple", getSmallestMultiple)
+
+
