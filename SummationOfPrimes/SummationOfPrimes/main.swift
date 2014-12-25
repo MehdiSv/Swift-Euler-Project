c7b2ff4f918373af605d33e7337621d7a8c24adf
Mehdi Sqalli <mehdisv@gmail.com>
2014-12-25 09:05:07 +0000

    Faster algorithm for LargestProductInSeries

---
+++ SummationOfPrimes/SummationOfPrimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SummationOfPrimes
+//
+//  Created by Mehdi Sqalli on 24/12/14.
+//  Copyright (c) 2014 MNT Developpement. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
