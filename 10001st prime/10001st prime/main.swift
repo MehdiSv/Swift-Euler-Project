d16b148b2c7dd87f0c1383c14e76613ce69681eb
Mehdi Sqalli <mehdisv@gmail.com>
2014-12-26 12:30:18 +0000

    Project Euler 10_001st Prime better performance using Sieve

--- 10001st prime/10001st prime/main.swift
+++ 10001st prime/10001st prime/main.swift
@@ -8,58 +8,104 @@
 
 import Foundation
 
-func isPrime(number:Int) -> Bool {
+/*  Using the isPrimeMethod */
+
+func isPrime(number:Int, primes: [Int]) -> Bool {
     
-    if number == 1 {
-        return false
-    }
-    else if number < 4 {
-        return true
-    }
-    else if number % 2 == 0 {
-        return false
-    }
-    else if number < 9 {
-        return true
-    }
-    else if number % 3 == 0 {
-        return false
-    }
-    else {
-        let maxPrime = Int(ceil(sqrt(Double(number))))
-        
-        for i in stride(from: 5, through: maxPrime, by: 6) {
-            if number % i == 0  || number % (i + 2) == 0 {
-                return false
-            }
+    let maxPrime = Int(ceil(sqrt(Double(number))))
+    for prime in primes {
+        if prime > maxPrime {
+            break
+        }
+        if number % prime == 0 {
+            return false
         }
     }
-    
     return true
 }
 
-func getNumberForXthPrime(prime:Int) -> Int {
+func nThPrime(var n : Int) -> Int {
     
-    var xThPrime = prime - 1 // We skip the prime 2 with the += 2
-    var number = 1
+    assert(n > 0, "argument must be positive")
     
-    while xThPrime > 0 {
-        
+    var primes = [2, 3]
+    
+    if n <= primes.count {
+        return primes[n - 1]
+    }
+    
+    var number = primes.last!
+    n -= primes.count
+    
+    while n > 0 {
         number += 2
-        if isPrime(number) {
-            xThPrime--
+        if isPrime(number, primes) {
+            primes.append(number)
+            n--
         }
     }
     
     return number
 }
 
-func printTimeElapsedWhenRunningCode(operation:(xThPrime:Int) -> Int) {
-    let startTime = CFAbsoluteTimeGetCurrent()
-    let number = operation(xThPrime: 10_001)
+/* */
+
+
+/*  Using the Sieve of Eratosthenes */
+
+func nthPrime(var n : Int) -> Int {
+    
+    assert(n > 0, "argument must be positive")
+    
+    // The below estimate is only valid for n > 6:
+    if n <= 6 {
+        return [2, 3, 5, 7, 11, 13][n-1]
+    }
+    
+    // Upper bound from http://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number:
+    let d = Double(n)
+    let upperBound = Int(d * (log(d) + log(log(d))))
+    
+    var composite = [Bool](count: upperBound + 1, repeatedValue: false)
+    
+    var x = 2
+    let maxPrime = Int(ceil(sqrt(Double(upperBound))))
+    
+    while x <= maxPrime {
+        if !composite[x] {
+            if (--n == 0) {
+                return x
+            }
+            for var y = x * x; y <= upperBound; y += x {
+                composite[y] = true
+            }
+        }
+        x++
+    }
+    while x <= upperBound {
+        if !composite[x] {
+            if (--n == 0) {
+                return x
+            }
+        }
+        x++
+    }
+    
+    assertionFailure("Fatal error")
+    return -1
+}
+
+func euler7() {
+    let number = nthPrime(13)
+    
     println(number)
+}
+
+func printTimeElapsedWhenRunningCode(operation:() -> ()) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    operation()
     let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
     println("Time elapsed : \(timeElapsed) s")
 }
 
-printTimeElapsedWhenRunningCode(getNumberForXthPrime)
+printTimeElapsedWhenRunningCode(euler7)
---
+++ LargestProductGrid/LargestProductGrid/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LargestProductGrid
+//
+//  Created by Mehdi Sqalli on 25/12/14.
+//  Copyright (c) 2014 MNT Developpement. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
