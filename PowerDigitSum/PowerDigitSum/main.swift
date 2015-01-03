22a3ce6bbd77ff7171e9b8eb5a54f68932814d10
MehdiSV <mehdisv@gmail.com>
2015-01-03 15:22:38 +0100

    Improve Performance

--- PowerDigitSum/PowerDigitSum/main.swift
+++ PowerDigitSum/PowerDigitSum/main.swift
@@ -8,5 +8,61 @@
 
 import Foundation
 
-println("Hello, World!")
+func addMulToResult(inout results:[Int], var mulResult:Int, index:Int) {
+    
+    if index == results.count {
+        results.append(0)
+    }
+    
+    mulResult = results[index] + mulResult
+    
+    var result = mulResult % 10
 
+    results[index] = result
+    
+    var retain = mulResult / 10
+    if retain > 0 {
+        addMulToResult(&results, retain, index + 1)
+    }
+}
+
+func mult(left:String, right:String) -> String {
+    
+    var results:[Int] = [Int]()
+    for (j, rightChar) in enumerate(reverse(right)) {
+     
+        let rightNumber = String(rightChar).toInt()!
+        
+        for (i, leftChar) in enumerate(reverse(left)) {
+            
+            let leftNumber = String(leftChar).toInt()!
+            
+            let mulResult = leftNumber * rightNumber
+            addMulToResult(&results, mulResult, i + j)
+        }
+    }
+    
+    return "".join(results.reverse().map() { String($0) } )
+}
+
+func powS(x:String, var power:Int) -> String {
+    
+    var result = x
+    while power > 1 {
+
+        result = mult(result, x)
+        power--
+    }
+    
+    return result
+}
+
+let powerSum =  powS("2", 1000)
+
+println(powerSum)
+
+let result = reduce(powerSum, 0) { (result, char) -> Int in
+    return String(char).toInt()! + result
+}
+
+println(result)
