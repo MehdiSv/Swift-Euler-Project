9e12ee6c1d7a611f771c4d1d5e9c1efb42ae1517
MehdiSV <mehdisv@gmail.com>
2015-01-03 15:21:41 +0100

    Add NumberLetterCounts

---
+++ NumberLetterCounts/NumberLetterCounts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NumberLetterCounts
+//
+//  Created by Mehdi Sqalli on 03/01/2015.
+//  Copyright (c) 2015 Mehdi Sqalli. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
